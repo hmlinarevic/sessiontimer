cd2af4f6e9b72a6a864b4f408be5d887788f41b1
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2023-12-14 10:26:36 +0100

    refactor: update where bg color is defined

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-"use client";
-
 import Timer from "@/components/timer";
 
 export default function Home() {
     return (
-        <main className="grid h-screen select-none content-center justify-center bg-[#1e1e1e]">
+        <main className="grid h-screen select-none content-center justify-center">
             <Timer />
         </main>
     );
